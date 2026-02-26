66eb18e39b457f0b193cdbf2a83e44a042657b6d
DianaDelacruzPerez <dianadelacruz@gmail.com>
2026-02-26 16:56:52 -0600

    Creamos una clase llamada ValidaUsuario

---
+++ SCPSAP-Negocio/Login/ValidaUsuario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCPSAP_Negocio.Login
+{
+    internal class ValidaUsuario
+    {
+    }
+}
--- SCPSAP-Negocio/obj/Debug/net10.0/SCPSAP-Negocio.AssemblyInfo.cs
+++ SCPSAP-Negocio/obj/Debug/net10.0/SCPSAP-Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCPSAP-Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578d7b31785b81abedaaec61a5bb4164e6d150aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e171ee14520eb8f752884585633a713dca7a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCPSAP-Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCPSAP-Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
