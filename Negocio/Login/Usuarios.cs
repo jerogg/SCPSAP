9197a6b162402b32017e740572e23f56c4c916c8
jerogg <jeroggg@gmail.com>
2026-03-01 15:09:08 -0600

    Creacion de clase en negocio

---
+++ Negocio/Login/Usuarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio.Login
+{
+    public class Usuarios
+    {
+        public bool ValidaUsuarios(string Nombre, string Contrasena)
+        {
+
+            try
+            {
+                if (Nombre == string.Empty && Contrasena == string.Empty)
+                {
+                    return false;
+                }
+                else
+                {
+                    //llamar metodo para validar el usuario en la capa de datos
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
